c99b82748edeab21b23d54eeede4b147c3f87732
1157047050 <41032810+1157047050@users.noreply.github.com>
2018-11-10 15:19:54 +0800

    Update MyRunable2.java

--- src/thread/MyRunable2.java
+++ src/thread/MyRunable2.java
@@ -13,7 +13,7 @@ public class MyRunable2 implements Runnable{
     @Override
     public void run() {
         for (int i=0;i<2000;i++) {
-            //System.out.println("============="+Thread.currentThread().getId()+"============");
+            //
            /* synchronized (list){
                 list();
             }*/
